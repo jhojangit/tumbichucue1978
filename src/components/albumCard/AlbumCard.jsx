5acadbcf8ed55f0f6a62ecaf1e384c569ea8eaa8
RazielBlaster <jhojanblackout@gmail.com>
2023-12-02 07:23:19 -0500

    Ready card album

--- src/components/albumCard/AlbumCard.jsx
+++ src/components/albumCard/AlbumCard.jsx
@@ -2,28 +2,29 @@ import React, { useState } from 'react';
 import './albumCard.css';
 
 const AlbumCard = ({ photo }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    };
 
-  return (
-    <div  className={`albumCard__container ${isExpanded ? 'expanded' : ''}`}
-          onClick={toggleExpand}>
-
-
-      <div className='albumCard__img'>
-        <img
-          loading='lazy'
-          src={photo.url}
-          alt={photo.title}
-          className={isExpanded ? 'fullScreen' : ''}
-        />
-      </div>
-      
-    </div>
-  );
+    return (
+        <div className={`albumCard__container ${isExpanded ? 'expanded' : ''}`} onClick={toggleExpand}>
+            <div className='albumCard__content'>
+                <div className='albumCard__img'>
+                    <img
+                        loading='lazy'
+                        src={photo.url}
+                        alt={photo.title}
+                        className={isExpanded ? 'fullScreen' : ''}
+                    />
+                </div>
+                {isExpanded && (
+                        <p className='albumCard__text'>{photo.title}</p>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default AlbumCard;
