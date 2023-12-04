b0c17b025dc45e67b40c89866bfcfa19459586e8
RazielBlaster <jhojanblackout@gmail.com>
2023-12-03 22:52:50 -0500

    Added information page

--- src/pages/information/Information.jsx
+++ src/pages/information/Information.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import './information.css'
 const Information = () => {
   return (
-    <div>Information</div>
+
+    <section className='information__container'>
+
+      <h2 className='information__title'>Constitución del Resguardo de Tumbichucue en el marco de la R. A INCORA 1978</h2>
+
+      <h4 className='information__subtitle'>
+        Resguardo Indígena de Tumbichucue.
+      </h4>
+
+      <p className='information__text'>
+          El Resguardo Nasa de Tumbichucue se encuentra ubicada al Norte del Municipio de Inzà y al nororiente del Departamento del Cauca a una distancia de 31 kms de la cabecera municipal; además, cuenta con una temperatura promedio que oscila entre17⁰ a 22⁰ C, su altura máximo es de 2200 mts  sobre el nivel del mar.
+
+          Se considera el mejor Resguardo de nuestra cultura Páez, es el más reciente de todos Títulos coloniales de 1700. Fue creado en 1978 bajo la reforma Agraria INCORA, y aun conserva toda su autenticidad e identidad cultural. Se ha dado una gran solides al Resguardo y a logrado la unión de propuestas que se reflejan en su capacidad de mantener la lengua materna Nasa Yuwe.
+      </p>
+    </section>
   )
 }
 
